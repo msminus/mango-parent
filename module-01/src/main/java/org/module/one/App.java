3395f88c1c1b7673a72a6176ddac095f5a9338be
Subrahmanyam Mamidi <35884419+msminus@users.noreply.github.com>
2023-07-16 18:41:14 -0500

    trying github dev for the first time

--- module-01/src/main/java/org/module/one/App.java
+++ module-01/src/main/java/org/module/one/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Git Hub Dev" );
+    }
+
+    public static void details() {
+        System.out.println("Codespace in cloud is next level");
     }
 }
